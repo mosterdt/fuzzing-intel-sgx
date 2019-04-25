b20dcaedd80d3fe187aedb813c92261a5f774a3e
Thomas De Backer <thomasisdebacker5@gmail.com>
2019-04-26 01:20:10 +0200

    cleaned up wrapper

--- fuzzer/main.c
+++ fuzzer/main.c
@@ -44,45 +44,31 @@ int seqp = 0;
 void fault_handler(void *base_adrs)
 {
 
-    //seq[seqp++%SEQ_LEN] = base_adrs;
-    if (fault_fired > 1) //ppv_pt - GET_PFN(ppv_pt) != 0xFFF && 
+    if (0 && fault_fired > 1) //ppv_pt - GET_PFN(ppv_pt) != 0xFFF && 
         mprotect(GET_PFN(ppv_pt), 4096, PROT_NONE);
     ppv_pt = pv_pt;
     pv_pt = base_adrs;
     mprotect(GET_PFN(base_adrs), 4096, PROT_WRITE|PROT_READ|PROT_EXEC);
-    printf("offset: %d, pfn=%p, ba=%p\n", (base_adrs - ebase_address) >> 12, GET_PFN(base_adrs),base_adrs );
+    printf("pfn=%p, ba=%p, offset: %d\n", GET_PFN(base_adrs), base_adrs, (base_adrs - ebase_address) >> 12 );
 
     fault_fired++;
 }
 
 void protect_memory(void *base_adrs, int size) {
     for (int i=0; i <= size; i += 0x1000) {
-        printf("protected address %p\n", base_adrs+i);
+        //printf("protected address %p\n", base_adrs+i);
         mprotect(base_adrs+i, 4096, PROT_NONE);
     }
+    printf("protected %d addresses\n", size/0x1000);
 }
 
 int main( int argc, char **argv )
 {
     sgx_enclave_id_t eid = create_enclave();
-    int rv = 1, secret = 0;
-    int cipher, plain;
 
-    /* ---------------------------------------------------------------------- */
     info("registering fault handler..");
     register_fault_handler(fault_handler);
 
-    /* ---------------------------------------------------------------------- */
-    info_event("Calling enclave..");
-    SGX_ASSERT_E( ecall_get_square_adrs(eid, &sq_pt) );
-    SGX_ASSERT_E( ecall_get_multiply_adrs(eid, &mul_pt) );
-    SGX_ASSERT_E( ecall_get_modpow_adrs(eid, &modpow_pt) );
-    modpow_pt = (void*) (((uint64_t) modpow_pt) & ~0xfff);
-    info("square at %p; muliply at %p; modpow at %p", sq_pt, mul_pt, modpow_pt);
-
-    SGX_ASSERT_E( ecall_rsa_encode(eid, &cipher, RSA_TEST_VAL) );
-    SGX_ASSERT_E( ecall_rsa_decode(eid, &plain, cipher) );
-    info("secure enclave encrypted '%d' to '%d'; decrypted '%d'", RSA_TEST_VAL, cipher, plain);
     info("base address = %p", get_enclave_base());
     info("base size= %p", get_enclave_size());
     info("tcs %p\n", sgx_get_tcs());
@@ -92,19 +78,38 @@ int main( int argc, char **argv )
     print_enclave_info();
 
 
-    for (int i=0; i < SEQ_LEN; i++)
-        seq[i] = 0;
 
-    protect_memory(get_enclave_base(), get_enclave_size());
+    /* ---------------------------------------------------------------------- */
+
+    int rv = 1, secret = 0;
+    int cipher, plain;
+
+
     getchar();
-    /*
-    mprotect(GET_PFN(modpow_pt), 4096, PROT_NONE);
-    mprotect(GET_PFN(sq_pt), 4096, PROT_NONE);
-    mprotect(GET_PFN(mul_pt), 4096, PROT_NONE);
-    */
-    SGX_ASSERT_E( ecall_rsa_decode(eid, &plain, cipher) );
-    printf("key is %s\n", seq);
+    protect_memory(get_enclave_base(), get_enclave_size());
+
+    SGX_ASSERT_E( ecall_get_square_adrs(eid, modpow_pt) );
+
     printf("pagefaults: %d\n", fault_fired);
+    fault_fired = 0;
+
+    getchar();
+    protect_memory(get_enclave_base(), get_enclave_size());
+
+    SGX_ASSERT_E( ecall_get_multiply_adrs(eid, modpow_pt) );
+
+    printf("pagefaults: %d\n", fault_fired);
+    fault_fired = 0;
+
+    getchar();
+    protect_memory(get_enclave_base(), get_enclave_size());
+
+    SGX_ASSERT_E( ecall_rsa_decode(eid, &plain, cipher) );
+
+    printf("pagefaults: %d\n", fault_fired);
+
+
+    /* ---------------------------------------------------------------------- */
 
 
     info_event("destroying SGX enclave");
